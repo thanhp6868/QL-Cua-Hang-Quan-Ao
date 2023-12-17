c8f8c8f7681c0a80b5b8ac723fa5d593a0c07acc
thanhp6868 <125439629+thanhp6868@users.noreply.github.com>
2023-12-17 22:36:44 +0700

    ok

--- .history/page/Men/product_20231213135623.php
+++ .history/page/Men/product_20231213135623.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-include 'db/connect.php';
-include 'backend/common_function.php';
+include('../../db/connect.php');
+include '../../backend/common_function.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,9 +13,9 @@ include 'backend/common_function.php';
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  <link rel="shortcut icon" href="img/fab.png" type="image/x-icon">
+  <link rel="stylesheet" href="../../style.css">
+
+  <link rel="shortcut icon" href="../../img/fab.png" type="image/x-icon">
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
     integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
@@ -27,15 +26,14 @@ include 'backend/common_function.php';
 
   <script type="text/javascript"
     src="https://res.cloudinary.com/veseylab/raw/upload/v1636192990/magicmouse/magic_mouse-1.2.1.cdn.min.js"></script>
-  <script src="js/js_own.js"></script>
+  <script src="../../js/js_own.js"></script>
 
-  <title>Home</title>
+  <title>HRX | Products</title>
 </head>
 
 <body>
-
   <header>
-    <div class="logo"></div>
+    <div class="logo"><a href="index.php"><img src="img/icon.png" alt="" srcset=""></a></div>
     <div>
       <div class="uper">
 
@@ -89,17 +87,15 @@ include 'backend/common_function.php';
           </div>
         </form>
 
-
       </div>
     </div>
-    <form action="page/Men/search_products.php" method="post"><input class="search" type="search" name="search_key"
-        id="" placeholder="Tìm kiếm sản phẩm">
-    </form>
+    <form action="search_products.php" method="post"><input class="search" type="search" name="search_key" id=""
+        placeholder="search for products, bands and more"></form>
 
     <div class="dropdown2">
       <?php
       if (isset($_SESSION['username'])) {
-        echo '<a href="user_area/profile.php" style="top:-20px; color: white; !important">
+        echo '<a href="../../user_area/profile.php" style="top:-20px; color: white; !important">
         <img src="img/profile.png" alt="" srcset=""  style="width:20px; !important">
         <p>Profile</p>
       </a>';
@@ -111,7 +107,7 @@ include 'backend/common_function.php';
 
       <div class="dropdown-content2" style="display:none" id="LoginForm">
 
-        <form action="backend/user_login.php" method="post">
+        <form action="../../backend/user_login.php" method="post">
           <div class="form-outline mb-4">
             <input type="username" id="form2Example1" class="form-control" name="username" placeholder="username" />
             <!-- <label class="form-label" for="form2Example1">Email address</label> -->
@@ -135,7 +131,7 @@ include 'backend/common_function.php';
 
             <div class="col">
 
-              <a href="page/forgot_password.php">Quên mật khẩu</a>
+              <a href="../forgot_password.php">Quên mật khẩu?</a>
             </div>
           </div>
 
@@ -144,15 +140,14 @@ include 'backend/common_function.php';
 
 
           <div class="text-center">
-            <p>Chưa có tài khoản <a href="page/reg.php"> Đăng ký </a></p>
-            <p> hoặc đăng nhập với: </p>
+            <p>Chưa có tài khoản? <a href="../reg.php">Đăng ký</a></p>
+            <p>hoặc đăng nhập bằng:</p>
             <button type="button" class="btn btn-link btn-floating mx-1">
               <i class="fab fa-facebook-f"></i>
-              <i class='bx bxl-facebook-circle fa-5x' style='color:#4b5af6'></i>
             </button>
 
             <button type="button" class="btn btn-link btn-floating mx-1">
-              <i class="fab fa-google fa-5x"></i>
+              <i class="fab fa-google"></i>
             </button>
 
             <button type="button" class="btn btn-link btn-floating mx-1">
@@ -174,7 +169,7 @@ include 'backend/common_function.php';
     <div class="uper  side2">
 
       <div class="dropdown headerimg">
-        <a href="page/cart.php">
+        <a href="../cart.php">
           <img src="img/cart.png" alt="" srcset=""><sup>
             <?php $cart = ItemInCart();
             echo $cart; ?>
@@ -184,99 +179,115 @@ include 'backend/common_function.php';
       </div>
     </div>
   </header>
-  <h1 class=bg_write>Bộ sưu tập</h1>
+  <div>
+    <h3 class="link_w"> <a href="../../index.php">Home /</a> <a href="../men.php">Men /</a>
+    </h3>
 
-  <div class="index_pro_show">
-    <h2>Sản phẩm mới</h2>
-    <div class="p_show">
-      <?php
-      $sql = "select * from product ";
-      $res = mysqli_query($conn, $sql);
-      if ($res) {
-        while ($row = mysqli_fetch_array($res)) {
-          $p_id = $row["p_id"];
-          $p_name = $row["p_name"];
-          $p_price = $row["p_price"];
-          $p_img = $row["p_img1"];
+  </div>
 
-          echo '<div class="ctag_u">
-          <a href="page/Men/product.php?id=' . $p_id . '">
-            <div class="c_img_bg_u ">
-              <img class="ctag_img_u" src="page/Men/img/' . $p_img . '" alt="">
-              <h3>' . $p_name . '</h3>
-              <h4>' . number_format($p_price, 0, ',', '.') . ' VNĐ' . '</h4>
-          </a>
-          <a href="add_to_cart.php?add_to_cart=' . $p_id . "/" . $p_price . '"><button class="add_to_cart"> Thêm vào giỏ hàng </button></a>
-        </div>';
-        }
-      } else {
-        die(mysqli_error($conn));
-      }
-      ?>
+  <!-- Fetch product details from database -->
+  <?php
+  // include('../../db/connect.php');
+  
+  if (isset($_GET['id'])) {
+    $p_id = $_GET['id'];
+    $sql = "select * from product where p_id='$p_id'";
+    $res = mysqli_query($conn, $sql);
+    if ($res) {
+      $row = mysqli_fetch_array($res);
+      $p_name = $row['p_name'];
+      $p_details = $row['p_details'];
+      $p_quantity = $row['p_quantity'];
+      $p_price = $row['p_price'];
+      $p_img1 = $row['p_img1'];
+      $p_img2 = $row['p_img2'];
+      $p_img3 = $row['p_img3'];
+      $p_img4 = $row['p_img4'];
+    } else {
+      die(mysqli_error($conn));
+    }
 
+  }
+
+  ?>
+
+
+  <!-- product details-->
+  <div class="image ">
+    <img class="none" src="img/<?php echo $p_img1; ?>" data-thumb="" alt="" />
+    <img class="none" src="img/<?php echo $p_img2; ?>" data-thumb="demo/images/up.jpg" alt="" title="" />
+    <img src="img/<?php echo $p_img3; ?>" data-thumb="demo/images/walle.jpg" alt="" data-transition="slideInLeft" />
+    <img src="img/<?php echo $p_img4; ?>" data-thumb="demo/images/nemo.jpg" alt="" title="" />
+  </div>
+
+
+
+
+
+
+
+  <div class="details">
+    <h1>
+      <?php echo $p_name;
+      echo "(số lượng còn:" . $p_quantity . ")" ?>
+    </h1>
+    <h2 class="detailsx">
+      <?php echo $p_details; ?>
+    </h2>
+    <button class="rateing"> <b>4.5*</b> <i>| 1.1k lượt mua</i> </button>
+
+    <h2 class="detailsy">
+      <?php echo number_format($p_price, 0, ',', '.') . ' VNĐ' ?>
+      <del>
+        <?php
+        echo number_format($p_price * 1.3, 0, ',', '.') . ' VNĐ';
+        ?>
+      </del>
+      <div class="orange">(Giá iu thương)</div>
+    </h2>
+    <h3>Chọn size <a href="#"> ></a></h3>
+
+
+
+    <div class="radio-toolbar">
+      <input type="radio" id="radio38" name="radioFruit" value="38">
+      <label class="mlxl" for="radio38">38</label>
+
+      <input type="radio" id="radio40" name="radioFruit" value="40">
+      <label for="radio40">40</label>
+
+      <input type="radio" id="radio42" name="radioFruit" value="42">
+      <label for="radio42">42</label>
+      <input type="radio" id="radio44" name="radioFruit" value="44">
+      <label for="radio44">44</label>
+      <input type="radio" id="radio46" name="radioFruit" value="46">
+      <label for="radio46">46</label>
     </div>
+    <p>&nbsp;</p>
+    <?php
+    if ($p_quantity > 0)
+    echo '<a href="../../add_to_cart.php?add_to_cart='.$p_id . '/' . $p_price.'"><button class="addbagb">Thêm
+    vào giỏ hàng</button></a>';
+  
+    else 
+    echo '<a href=""><button class="addbagb">Hết hàng</button></a>';
+    
 
-  </div>
-  <div class="deal_of_the_day">
-    <h1>Một số sản phẩm khác</h1>
+    ?>
 
-  </div>
+    <!-- <button class="wishlistb">WISHLIST</button> -->
 
-  <!-- Catagoris start -->
-  <div class="catag reveal">
-    <a href="page/men.php">
-      <div class="c_img_bg">
-        <img src="img/men.png" alt="">
-        <h3>Nam</h3>
-
-      </div>
-    </a>
-    <a href="page/Men/all_product.php?ctg=Woman">
-      <div class="c_img_bg">
-        <img src="img/woman.png" alt="">
-        <h3>Nữ</h3>
-      </div>
-    </a>
-    <a href="page/Men/all_product.php?ctg=Bagpack">
-      <div class="c_img_bg">
-        <img class="bag_img" src="img/bag.png" alt="">
-        <h3>Balo</h3>
-      </div>
-    </a>
-    <a href="page/Men/all_product.php?ctg=shoes">
-      <div class="c_img_bg">
-        <img src="img/shoes.png" alt="">
-        <h3>Giày</h3>
-      </div>
-    </a>
-    <a href="page/Men/all_product.php?ctg=Asscories">
-      <div class="c_img_bg ">
-        <img src="img/asscories.png" alt="">
-        <h3>Vòng tay</h3>
-      </div>
-    </a>
-    <a href="page/Men/all_product.php?ctg=Wareable">
-      <div class="c_img_bg">
-        <img src="img/watch.png" alt="">
-        <h3>Đồng hồ</h3>
-      </div>
-    </a>
-    <a href="page/Men/all_product.php?ctg=Sunglass">
-      <div class="c_img_bg">
-        <img src="img/sunglass1.png" alt="">
-        <h3>Kính</h3>
-      </div>
-    </a>
-    <a href="page/Men/all_product.php?ctg=Fitness">
-      <div class="c_img_bg ">
-        <img class="fitness" src="img/fitness1.png" alt="">
-        <h3>GYM</h3>
-      </div>
-    </a>
 
 
   </div>
+  <div class="productsdetails">
+    <h2 class="productsdetailsh2">Mô tả sản phẩm</h2>
 
+    <h3>Mô tả sản phẩm</h3>
+
+    <h3>100 % Cotton co giãn hai chiều, vip</h3>
+
+  </div>
 
 
 
@@ -290,17 +301,36 @@ include 'backend/common_function.php';
       }
     }
   </script>
+  <!-- footer start-->
 
 
 
 
 
 
+  <footer>
+    <div class="footermain">
+      <div class="app">
+        <a href="#">
+          <img src="img/google_play.png" alt="" srcset="">
+        </a>
+        <br>
+        <a href="#">
+          <img src="img/apple_store.png" alt="" srcset="">
+        </a>
+      </div>
 
-  <!-- Footer File call -->
-  <?php
-  include 'footer.php';
-  ?>
+      <div class="social">
+        <ul>
+          <li><a href="#"><img src="img/fb.png" alt="" srcset=""></a></li>
+          <li><a href="#"><img src="img/insta.png" alt="" srcset=""></a></li>
+          <li><a href="#"><img src="img/twitter.png" alt="" srcset=""></a></li>
+          <li><a href="#"><img src="img/link.png" alt="" srcset=""></a></li>
+        </ul>
+      </div>
+
+    </div>
+  </footer>
 </body>
 
 </html>
